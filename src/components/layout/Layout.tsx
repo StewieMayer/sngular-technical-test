cee2bf1b6ebda3a002cf703352cc70f92b1e80cb
stewie <stewiemayer@hotmail.com>
2025-08-06 13:07:55 -0600

    style: se modifican los estilos

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex flex-col min-h-screen bg-radial from-blue-100 to-blue-500">
       <Header />
-      <main className="flex flex-1 items-center justify-center container mx-auto my-auto px-4 py-8 max-w-7xl">
+      <main className="flex flex-1 items-center justify-center px-4 py-8 min-w-screen font-sans">
         {children}
       </main>
       <Footer />
