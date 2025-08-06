306612efcdf8b2e636a787325f50820b41367859
stewie <stewiemayer@hotmail.com>
2025-08-06 15:03:10 -0600

    style: se modifica el bg de header y se vuelve flex-col la card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 const Card= ({children}:PropsWithChildren) => {
   return (
-    <div className="bg-white p-8 rounded-xl shadow-2xl w-full max-w-lg">
+    <div className="flex-col bg-white p-8 rounded-xl shadow-2xl w-full max-w-lg">
       {children}
     </div>
   );
--- src/components/layout/components/Header.tsx
+++ src/components/layout/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-blue-500 shadow-md">
+    <header className="flex justify-between items-center bg-blue-500 shadow-md">
       <div className="flex items-center">
-        <h1 className="text-white text-xl font-semibold">
+        <h1 className="bg-green-500 p-4 text-white text-xl font-semibold">
           Prueba Técnica Sngular
         </h1>
       </div>
