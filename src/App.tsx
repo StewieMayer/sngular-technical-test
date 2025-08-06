f5c36de04ea57767ace7c0fe5e965daa855da7ec
stewie <stewiemayer@hotmail.com>
2025-08-06 13:08:38 -0600

    refactor: se separa en diferentes componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Card from "./components/Card";
 import Layout from "./components/layout/Layout";
+import SerieDescription from "./components/SerieDescription";
+import SerieForm from "./components/SerieForm";
+import SerieResult from "./components/SerieResult";
 
 const App: React.FC = () => {
   return (
     <Layout>
-      <Card />
+      <Card>
+        <SerieDescription />
+        <SerieForm />
+        <SerieResult />
+      </Card>
     </Layout>
   );
 };
